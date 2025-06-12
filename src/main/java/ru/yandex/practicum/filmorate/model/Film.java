4e8bca60f5aff9d76dbe3c71d9f4ce7f05b85f5e
AndreyAA-code <otd53@yandex.ru>
2025-06-12 09:29:59 +0300

    add User and Film classes

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,20 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.sql.Date;
+import java.time.Duration;
 
 /**
  * Film.
  */
-@Getter
-@Setter
+@Data
 public class Film {
+    private long id;
+    private String name;
+    private String description;
+    private Date releaseDate;
+    private Duration duration;
 }
