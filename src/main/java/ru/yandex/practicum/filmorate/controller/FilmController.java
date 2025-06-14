ffb8541183d0bd5ac7dc13ede2ceee3e7e99c8fb
AndreyAA-code <otd53@yandex.ru>
2025-06-14 22:51:32 +0300

    add User and Film classes postman tests passed slf4g for FilmController generated

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,11 +1,11 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
-
 import java.sql.Date;
-import java.time.Duration;
 import java.time.LocalDate;
 import java.util.Collection;
 import java.util.HashMap;
@@ -15,38 +15,50 @@ import java.util.Map;
 @RequestMapping("/films")
 public class FilmController {
 
+    private final static Logger log = LoggerFactory.getLogger(FilmController.class);
     private final Map<Long, Film> films = new HashMap<>();
 
     @GetMapping
     public Collection<Film> findAll() {
+        log.debug("Find all films");
         return films.values();
     }
 
     @PostMapping
     public Film createFilm(@RequestBody Film film) {
+        log.debug("Create film: {} started", film);
+        log.debug("Film: {} send to validation", film);
         validateFilm(film);
-        System.out.println(film.getDuration());
-
+        log.debug("Film: {} successfully validated", film);
         film.setId(getNextId());
+        log.debug("Film: {} created with id: {}", film, film.getId());
         films.put(film.getId(), film);
+        log.info("Film: {} successfully created with id: {}", film, film.getId());
         return film;
     }
 
     @PutMapping
     public Film updateFilm(@RequestBody Film newFilm) {
+        log.debug("Update film: {} started", newFilm);
         if (newFilm.getId() == null) {
+            log.debug("Film: {} with Id: {} not found in the database", newFilm, newFilm.getId());
             throw new ValidationException("id должен быть указан");
         }
-        validateFilm(newFilm);
+
         if (films.containsKey(newFilm.getId())) {
+            log.debug("Film: {} with Id: {} found in the database", newFilm, newFilm.getId());
             Film oldFilm = films.get(newFilm.getId());
+            log.debug("Film: {} send to validation", newFilm);
             validateFilm(newFilm);
+            log.debug("Film: {} successfully validated", newFilm);
             oldFilm.setName(newFilm.getName());
             oldFilm.setDescription(newFilm.getDescription());
             oldFilm.setReleaseDate(newFilm.getReleaseDate());
             oldFilm.setDuration(newFilm.getDuration());
+            log.debug("Film: {} info successfully updated", oldFilm);
             return oldFilm;
         }
+        log.debug("Film: {} with Id: {} not found in the database", newFilm, newFilm.getId());
         throw new ValidationException("Такого Id нет");
     }
 
@@ -72,6 +84,7 @@ public class FilmController {
             .mapToLong(id->id)
             .max()
             .orElse(0L);
+    log.debug("New id: {} succesfully generated", ++currentMaxId);
     return ++currentMaxId;
     }
 }
