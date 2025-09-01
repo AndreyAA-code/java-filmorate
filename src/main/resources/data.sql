1d79b5f231b045b4d49bfbbd09f2c4c9e0989e1c
AndreyAA-code <otd53@yandex.ru>
2025-09-01 14:17:56 +0300

    add DB scheme Завершено ревью пром задания ФЗ 12 добавить БД H2 добавить схему для создания таблиц добавить схему для создания словарей - рейтинг, жанр, статус дружбы

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,29 @@
-INSERT INTO rating (name) VALUES ('G'), ('PG'), ('PG-13'), ('R'), ('NC-17');
-INSERT INTO genre (name) VALUES ('Комедия'), ('Драма'), ('Мультфильм'), ('Триллер'), ('Документальный'), ('Боевик');
-INSERT INTO friendship_status (status) VALUES ('Запрос'), ('Подтверждена');
+MERGE into rating AS target
+    USING (VALUES ('G'),
+               ('PG'),
+               ('PG-13'),
+               ('R'),
+               ('NC-17')
+        ) AS source (name)
+ON target.name = source.name
+WHEN NOT MATCHED THEN
+    INSERT (name) VALUES (source.name);
+
+MERGE into genre AS target
+    USING (VALUES ('Комедия'),
+                  ('Драма'),
+                  ('Мультфильм'),
+                  ('Триллер'),
+                  ('Документальный')
+        ) AS source (name)
+ON target.name = source.name
+WHEN NOT MATCHED THEN
+    INSERT (name) VALUES (source.name);
+
+MERGE into FRIENDSHIP_STATUS AS target
+    USING (VALUES ('Запрос'),
+                  ('Подтверждена')
+        ) AS source (status)
+ON target.STATUS = source.status
+WHEN NOT MATCHED THEN
+    INSERT (STATUS) VALUES (source.status);
