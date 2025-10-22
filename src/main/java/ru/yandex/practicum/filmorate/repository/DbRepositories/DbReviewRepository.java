d082d7bdb8451b1ec6dc62abc0d87ddd9d975ae6
AndreyAA-code <otd53@yandex.ru>
2025-10-22 16:28:46 +0300

    checked all found errors in 13 sprint postman tests

--- src/main/java/ru/yandex/practicum/filmorate/repository/DbRepositories/DbReviewRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/repository/DbRepositories/DbReviewRepository.java
@@ -29,7 +29,6 @@ public class DbReviewRepository implements ReviewRepository {
     private static final String FIND_REVIEW_BY_ID_QUERY = "SELECT * FROM reviews WHERE review_id = ?";
     private static final String FIND_USERID_FROM_REVIEW_QUERY = "SELECT user_id FROM reviews WHERE review_id = ?";
     private static final String CREATE_REVIEW_QUERY = "INSERT INTO reviews (content, isPositive, user_id, film_id) VALUES (?, ?, ?, ?)";
-//  private static final String UPDATE_REVIEW_QUERY = "UPDATE reviews SET content = ?, isPositive = ?, user_id = ?, film_id = ? WHERE review_id =?";
     private static final String UPDATE_REVIEW_QUERY = "UPDATE reviews SET content = ?, isPositive = ? WHERE review_id =?";
     private static final String DELETE_REVIEW_QUERY = "DELETE FROM reviews WHERE review_id = ?";
     private static final String GET_REVIEWS_BY_FILM_ID_QUERY = "SELECT reviews.*, COALESCE(SUM(reviews_users.useful),0) AS useful_sum\n" +
@@ -76,8 +75,6 @@ public class DbReviewRepository implements ReviewRepository {
 
     @Override
     public Review updateReview(Review newReview) {
-        //jdbc.update(UPDATE_REVIEW_QUERY, newReview.getContent(), newReview.getIsPositive(), newReview.getUserId(),
-        //        newReview.getFilmId(), newReview.getReviewId());
         jdbc.update(UPDATE_REVIEW_QUERY, newReview.getContent(), newReview.getIsPositive(), newReview.getReviewId());
         jdbc.update(DELETE_ALL_LIKES_DISLIKES_FOR_REVIEW, newReview.getReviewId());
         Long reviewUserId = jdbc.queryForObject(FIND_USERID_FROM_REVIEW_QUERY, Long.class, newReview.getReviewId()  );
--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -84,8 +84,6 @@ public class FilmService {
     }
 
     public Review updateReview(Review review) {
-        //userRepository.getUserById(review.getUserId());
-       // filmRepository.getFilmById(review.getFilmId());
         return reviewRepository.updateReview(review);
     }
 
