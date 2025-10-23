b03b6f56f6571dcc71c23ea544fa6aa750020af3
AndreyAA-code <otd53@yandex.ru>
2025-10-23 12:27:58 +0300

    added @Slf4j logs to services and DB repositories

--- src/main/java/ru/yandex/practicum/filmorate/repository/mappers/UserEventRowMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/repository/mappers/UserEventRowMapper.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.repository.mappers;
 
-import lombok.AllArgsConstructor;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.EventType;
